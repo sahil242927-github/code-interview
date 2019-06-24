b55721d140198ba9f5993bf1d80fae51fb3a4aff
Mohd Sahil <mohdsahil@Mohds-MacBook.local>
2019-06-24 10:52:30 +0530

    remove duplicate array

--- trickyQuestions/Q8--math.max().js
+++ trickyQuestions/Q8--math.max().js
@@ -1,7 +1,7 @@
 /*
  Math.max() => Return the number with the highest value
 */
-console.log(Math.max(1, 2, 3)); // returns 2
+console.log(Math.max(1, 2, 3)); // returns 3
 
 /* Note: if you don't pass any number it will return -1  */
 console.log(Math.max()); // return -1 or -infinity
