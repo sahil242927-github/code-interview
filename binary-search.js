5dd2f6416e2b39beb34f73dd3501b5c48ed32efd
sahil-github29 <sahil242927@gmail.com>
2020-03-07 06:20:57 +0530

    binary search

---
+++ binary-search.js
@@ -0,0 +1,25 @@
+/* 
+ Given a sorted array of interger, write a function that accepts a value and returns the index
+ where the value passed to the function is located. if the value if not found return -1
+ banarySearch([1,2,3,4,5,6,7,8], 3) // 4
+*/
+
+function banarySearch(arr, val) {
+  let min = 0;
+  let max = arr.length - 1;
+
+  while (min <= max) {
+    let middle = Math.floor((min + max) / 2);
+
+    if (arr[middle] < val) {
+      min = middle + 1;
+    } else if (arr[middle] > val) {
+      max = middle - 1;
+    } else {
+      return middle;
+    }
+  }
+  return -1;
+}
+
+console.log(banarySearch([1, 2, 3, 4, 5, 6, 7, 8], 8));
