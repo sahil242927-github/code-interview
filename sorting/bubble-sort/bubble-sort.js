ef7171a8342a47a0fd93a2349fb396d071cabc73
sahil-github29 <sahil242927@gmail.com>
2020-03-12 11:47:41 +0530

    bubble sort fixes

--- sorting/bubble-sort/bubble-sort.js
+++ sorting/bubble-sort/bubble-sort.js
@@ -1,9 +1,8 @@
 const arr = [14, 9, 4, 3, 27, 20];
 
 function bubbleSort(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length - 1; j++) {
-      console.log(arr, arr[j], arr[j + 1]);
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
