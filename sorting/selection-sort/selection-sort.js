cba65229231b406413705f979b7e0addf7219488
sahil-github29 <sahil242927@gmail.com>
2020-03-13 00:31:04 +0530

    selection sort

---
+++ sorting/selection-sort/selection-sort.js
@@ -0,0 +1,20 @@
+const arr = [30, 24, 19, 27, 43];
+
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[lowest]) {
+        lowest = j;
+      }
+    }
+    if (i !== lowest) {
+      let temp = arr[i];
+      arr[i] = arr[lowest];
+      arr[lowest] = temp;
+    }
+  }
+}
+
+selectionSort(arr);
+console.log(arr);
