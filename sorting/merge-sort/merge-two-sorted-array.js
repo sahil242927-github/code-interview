9f7f45c52e2c9550c84cc0e7380d81995f554ce2
sahil-github29 <sahil242927@gmail.com>
2020-03-16 13:16:54 +0530

    merged two sorted array improved version

--- sorting/merge-sort/merge-two-sorted-array.js
+++ sorting/merge-sort/merge-two-sorted-array.js
@@ -2,26 +2,27 @@ const arr1 = [1, 10, 50];
 const arr2 = [2, 14, 99, 100];
 
 function mergeTwoSortedArray(arr1, arr2) {
-  let smallArray = arr1.length < arr2.length ? arr1.length : arr2.length;
   let i = 0;
   let j = 0;
   const mergedArr = [];
-  while (smallArray >= 0) {
-    console.log(arr1[i], arr2[j], i);
+  while (i < arr1.length && i < arr2.length) {
     if (arr1[i] > arr2[j]) {
-      if (arr2[j]) mergedArr.push(arr2[j]);
+      mergedArr.push(arr2[j]);
       j++;
     } else {
-      if (arr1[i]) mergedArr.push(arr1[i]);
+      mergedArr.push(arr1[i]);
       i++;
-      smallArray--;
     }
   }
   // if arrays have different length then merge the rest of the elements of biggest array
-  smallArray === arr2.length
-    ? mergedArr.push(...arr1.splice(smallArray - 1))
-    : mergedArr.push(...arr2.splice(smallArray - 1));
-
+  while (i < arr1.length) {
+    mergedArr.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    mergedArr.push(arr2[j]);
+    j++;
+  }
   return mergedArr;
 }
 
