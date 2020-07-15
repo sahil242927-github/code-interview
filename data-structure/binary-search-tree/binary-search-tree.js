d0009aa9b9bfae13901f2616931d5ffb6a173c62
Mohd Sahil <sahil242927@gmail.com>
2020-07-15 16:16:35 +0530

    BFS completed

---
+++ data-structure/binary-search-tree/binary-search-tree.js
@@ -0,0 +1,140 @@
+// --- Directions
+// 1) Implement the Node class to create
+// a binary search tree.  The constructor
+// should initialize values 'data', 'left',
+// and 'right'.
+// 2) Implement the 'insert' method for the
+// Node class.  Insert should accept an argument
+// 'data', then create an insert a new node
+// at the appropriate location in the tree.
+// 3) Implement the 'contains' method for the Node
+// class.  Contains should accept a 'data' argument
+// and return the Node in the tree with the same value.
+// If the value isn't in the tree return null.
+
+// This BinarySearchtree example is by colt steele
+
+class BinarySearchtree {
+  constructor() {
+    this.root = null;
+  }
+
+  find(value) {
+    if (this.root === null) return null;
+
+    let current = this.root,
+      found = false;
+
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    if (!found) return null;
+    return current;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+
+    // checking if there is a root already
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      // we will update the current as we go
+      let current = this.root;
+      while (true) {
+        if (value === current.value) return undefined;
+        if (value < current.value) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (value > current.value) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
+
+  BFS() {
+    let data = [],
+      node = this.root,
+      queue = [];
+
+    // place the root node in the Queue
+    queue.push(node);
+
+    // while the queue isn't empty we will shift the queue and add it to the visited array
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.value);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  }
+}
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  /* insert(data) {
+    if (data < this.data && this.left) {
+      this.left.insert(data);
+    } else if (data < this.data) {
+      this.left = new Node(data);
+    } else if (data > this.data && this.right) {
+      this.right.insert(data);
+    } else if (data > this.data) {
+      this.right = new Node(data);
+    }
+  } */
+
+  /* contains(data) {
+    if (this.data === data) {
+      return this;
+    }
+
+    if (this.data < data && this.right) {
+      return this.right.contains(data);
+    } else if (this.data > data && this.left) {
+      return this.left.contains(data);
+    }
+
+    return null;
+  } */
+}
+
+const tree = new BinarySearchtree();
+//tree.root = new Node(20);
+// tree.root.right = new Node(15);
+// tree.root.left = new Node(7);
+// tree.root.left.right = new Node(9);
+
+tree.insert(10);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
+
+console.log(tree.BFS());
+
+module.exports = Node;
